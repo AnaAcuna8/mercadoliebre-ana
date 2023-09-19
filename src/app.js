b136410cf1097311bc681913d8d69148ddd152ac
Ana Acuña <ana.081296@hotmail.com>
2023-09-19 20:07:36 -0300

    first commit

---
+++ src/app.js
@@ -0,0 +1,24 @@
+const express = require('express');
+const path = require('path');
+
+const app = express();
+const port = process.env.PORT || 3000;
+
+/* para css*/
+app.use(express.static(path.join(__dirname, "/public")));
+
+app.get("/", (req, res) => {
+    res.sendFile( path.join(__dirname, "/views/index.html") );
+});
+
+app.get("/register", (req, res) => {
+    res.sendFile( path.join(__dirname, "/views/register.html") );
+});
+
+app.get("/login", (req, res) => {
+    res.sendFile( path.join(__dirname, "/views/login.html") );
+});
+
+app.listen(port, () => {
+    console.log(`Servidor iniciado en http://localhost:${port}`);
+});
